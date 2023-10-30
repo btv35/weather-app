0e0cdacdea63ca35d3f3d92bdab6717818635206
Tanya Bronnikova <btv353296@gmail.com>
2023-10-31 00:03:43 +0200

    Updated time section

--- src/app.js
+++ src/app.js
@@ -2,23 +2,43 @@ let apiKey = "9496e550c50357215588180769f9651c";
 let city = "Kyiv";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+function formateDate(timestemp) {
+  let date = new Date(timestemp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showCity(response) {
-  console.log(response.data);
-  
+  //   console.log(response.data);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
   let descriptionElement = document.querySelector("#desription");
-  descriptionElement.innerHTML = response.data.weather[0].description;
-
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-
   let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#date");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   humidityElement.innerHTML = response.data.main.humidity;
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 axios.get(apiURL).then(showCity);
